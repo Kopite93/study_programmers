d7a30596f0805002a40e78f2c3317d0a9cc14f1c
Kopite93 <1000nulee@naver.com>
2022-03-13 17:52:48 +0900

    study

--- 12918.js
+++ 12918.js
@@ -1,11 +1,33 @@
 function solution(s) {
-  return s.length == 4 || s.length == 6 ? Boolean(Number(s)) : false;
+  const len = s.length;
+
+  if (len === 4 || len === 6) {
+    return s.split("").every((c) => !isNaN(c));
+  }
+  return false;
 }
 
-function solution(s) {
-  if (s.length == 4 || s.length == 6) {
-    return !isNaN(s);
-  } else {
-    return false;
-  }
-}
+
+// function solution(s) {
+//   if (!(s.length == 4 || s.length == 6)) {
+//     return false;
+//   } else {
+//     const tmp = s.split("");
+//     for (let i = 0; i < tmp.length; i++) {
+//       if (isNaN(tmp[i])) {
+//         return false;
+//       }
+//     }
+//   }
+//   return true;
+// }
+
+// function solution(s) {
+//   let result = parseInt(s);
+//   if ((s.length === 4 || s.length === 6) && s == result) {
+//     result = true;
+//   } else {
+//     result = false;
+//   }
+//   return result;
+// }
