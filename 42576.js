d6ae7f3c68ac1029e9cac0498b6ef85e1203c85e
Kopite93 <1000nulee@naver.com>
2022-12-20 23:10:15 +0900

    221220

--- 42576.js
+++ 42576.js
@@ -1,9 +1,33 @@
+const p = ["mislav", "stanko", "mislav", "ana"];
+const c = ["stanko", "ana", "mislav"];
+
 function solution(p, c) {
   p.sort();
   c.sort();
-  for (i = 0; i < p.length; i++) {
+
+  for (let i = 0; i < p.length; i++) {
     if (p[i] !== c[i]) {
       return p[i];
     }
   }
+}
+
+console.log(solution(p, c));
+
+function solution(participant, completion) {
+  const map = new Map();
+
+  for (let i = 0; i < participant.length; i++) {
+    let a = participant[i],
+      b = completion[i];
+
+    map.set(a, (map.get(a) || 0) + 1);
+    map.set(b, (map.get(b) || 0) - 1);
+  }
+
+  for (let [k, v] of map) {
+    if (v > 0) return k;
+  }
+
+  return "nothing";
 }
