9ca68f47797aff6295878789926d10e656c70557
Kopite93 <1000nulee@naver.com>
2022-12-23 01:56:03 +0900

    221223

---
+++ 1845.js
@@ -0,0 +1,25 @@
+const nums = [3, 1, 2, 3];
+
+function solution(nums) {
+  const maxSelect = nums.length / 2;
+  const set = new Set(nums);
+
+  const newArr = [...set];
+
+  if (Math.floor(maxSelect) > newArr.length) {
+    return newArr.length;
+  } else if (Math.floor(maxSelect) <= newArr.length) {
+    return Math.floor(maxSelect);
+  }
+}
+
+// 다른 풀이
+
+function solution(nums) {
+  const max = nums.length / 2;
+  const arr = [...new Set(nums)];
+
+  return arr.length > max ? max : arr.length;
+}
+
+// set.size 속성 알아보기
